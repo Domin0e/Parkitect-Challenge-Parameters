4ffebda86cd374a486579bdde1eb634bbb32a73b
Domin0e <domin0e@domin0e.com>
2019-05-01 16:32:01 +0200

    Initial Commit

---
+++ ParkParams/ParkParams/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParkParams
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RollTheme.loadLists();
+        }
+
+        private void buttonRollAll_Click(object sender, RoutedEventArgs e)
+        {
+            textboxTheme.Text = RollTheme.RollForTheme();
+            System.Threading.Thread.Sleep(1);
+            textboxModOne.Text = RollTheme.RollForModifier();
+            System.Threading.Thread.Sleep(1);
+            textboxModTwo.Text = RollTheme.RollForModifier();
+            System.Threading.Thread.Sleep(1);
+            textboxModThree.Text = RollTheme.RollForModifier();
+        }
+
+        private void buttonRollMods_Click(object sender, RoutedEventArgs e)
+        {
+            textboxModOne.Text = RollTheme.RollForModifier();
+            System.Threading.Thread.Sleep(1);
+            textboxModTwo.Text = RollTheme.RollForModifier();
+            System.Threading.Thread.Sleep(1);
+            textboxModThree.Text = RollTheme.RollForModifier();
+        }
+
+        private void buttonRollModOne_Click(object sender, RoutedEventArgs e)
+        {
+            textboxModOne.Text = RollTheme.RollForModifier();
+        }
+
+        private void buttonRollModTwo_Click(object sender, RoutedEventArgs e)
+        {
+            textboxModTwo.Text = RollTheme.RollForModifier();
+        }
+
+        private void buttonRollModThree_Click(object sender, RoutedEventArgs e)
+        {
+            textboxModThree.Text = RollTheme.RollForModifier();
+        }
+    }
+}
---
+++ ParkParams/ParkParams/RollTheme.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.IO;
+
+namespace ParkParams
+{
+    static class RollTheme
+    {
+        static public List<string> themes, modifiers, buildreq, guestmod, rides, centerpiece = new List<string>();
+        static Random rand = new Random();
+
+        static public void loadLists()
+        {
+            string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\Lists\"; //Process.GetCurrentProcess().MainModule.FileName
+            
+            string filter = "Themes.txt";
+            string[] files = Directory.GetFiles(folder, filter);
+            themes = File.ReadAllLines(files[0]).ToList();
+
+            filter = "Modifiers.txt";
+            files = Directory.GetFiles(folder, filter);
+            modifiers = File.ReadAllLines(files[0]).ToList();
+
+            filter = "Build Requirements.txt";
+            files = Directory.GetFiles(folder, filter);
+            buildreq = File.ReadAllLines(files[0]).ToList();
+
+            filter = "Guest Modificators.txt";
+            files = Directory.GetFiles(folder, filter);
+            guestmod = File.ReadAllLines(files[0]).ToList();
+
+            filter = "Rides.txt";
+            files = Directory.GetFiles(folder, filter);
+            rides = File.ReadAllLines(files[0]).ToList();
+            
+            filter = "Centerpiece.txt";
+            files = Directory.GetFiles(folder, filter);
+            centerpiece = File.ReadAllLines(files[0]).ToList();
+        }
+        static public string RollForTheme()
+        {
+            return themes[rand.Next(themes.Count)];
+        }
+
+        static public string RollForModifier()
+        {
+            string modifier = modifiers[rand.Next(modifiers.Count)];
+
+            switch (modifier)
+            {
+                case "Build Requirement:":
+                    modifier += RollforBuild();
+                    break;
+                case "Guest Modifier:":
+                    modifier += RollforGuest();
+                    break;
+                case "Ride Requirement:":
+                    modifier += RollforRide();
+                    break;
+                case "Centerpiece Requirement:":
+                    modifier += RollforRide();
+                    break;
+            } 
+            
+            return modifier;
+        }
+
+        static private string RollforBuild()
+        {
+            return " " + buildreq[rand.Next(buildreq.Count)];
+        }
+
+        static private string RollforGuest()
+        {
+            return " " + guestmod[rand.Next(guestmod.Count)];
+        }
+
+        static private string RollforRide()
+        {
+            return " " + rides[rand.Next(rides.Count)];
+        }
+
+        static private string RollforWeenie()
+        {
+            string returnstring = centerpiece[rand.Next(rides.Count)];
+            if (returnstring == "Ride")
+            {
+                returnstring = RollforRide();
+            }
+            return " " + returnstring;
+        }
+    }
+}
